c259a73526c3db040841c7b81cb9f9d5797df802
user.email <choit@LAPTOP-RVEN208A>
2024-03-28 20:11:17 +0900

    課題1.19クラスの基本の提出

---
+++ curriculum_A/src/basicClass/Dog.java
@@ -0,0 +1,18 @@
+package basicClass;
+
+public class Dog {
+
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String inu;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int number;
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Dog() {
+		this.inu = "犬";
+	}
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	public Dog(String inu, int number) {
+		this.inu = inu;
+		this.number = number;
+	}
+}
